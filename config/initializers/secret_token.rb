faf50b9e079371012e8ee8a5a8999269993991fc
Abraham Stimson <abraham.stimson@gmail.com>
2013-01-16 17:50:46 -0600

    Clean start.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checklistapp::Application.config.secret_token = '81ebf0ac9446d2a8e2f440e1ca602b9d3500bacd8a44b91d63fadae21e38633c7763441500ca95575dd78f778925ed554f042f20a6d3a08546ffd42c2b98684a'
